471796267169f2102b8b33c406a211fea10fd9ab
Ritwik Pathak <107452129+f1rstCoder@users.noreply.github.com>
2023-01-16 11:13:33 +0530

    Update NewsItem.js

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,24 +1,35 @@
-import React from 'react'
-const NewsItem = (props) => {
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-    let { title, description, imageUrl, newsUrl, author, date, source } = props;
+
+const Navbar = () => {
+
     return (
-        <div className='my-3'>
-            <div className="card">
-                <div style={{ display: 'flex', justifyContent: 'flex-end', position: 'absolute', right: '0' }}>
-                    <span className=" badge rounded-pill bg-dark">{source}</span>
+        <div>
+            <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
+                <div className="container-fluid">
+                    <Link className="navbar-brand" to="/">Khabar</Link>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+                            </li>
+                            <li className="nav-item"><Link className="nav-link" to="/business">Business</Link></li>
+                            <li className="nav-item"><Link className="nav-link" to="/entertainment">Entertainment</Link></li>
+                            <li className="nav-item"><Link className="nav-link" to="/health">Health</Link></li>
+                            <li className="nav-item"><Link className="nav-link" to="/science">Science</Link></li>
+                            <li className="nav-item"><Link className="nav-link" to="/sports">Sports</Link></li>
+                            <li className="nav-item"><Link className="nav-link" to="/technology">Technology</Link></li>
+                        </ul>
+                    </div>
                 </div>
-                <img src={imageUrl} className="card-img-top" alt="..." />
-                <div className="card-body">
-                    <h5 className="card-title">{title}...</h5>
-                    <p className="card-text">{description}...</p>
-                    <p className="card-text"><small className="text-muted">By {author ? author : "Unknown"} on {new Date(date).toLocaleString(undefined, { timeZone: 'Asia/Kolkata' })}</small></p>
-                    <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
-                </div>
-            </div>
+            </nav>
         </div>
     )
 
 }
 
-export default NewsItem
+export default Navbar
